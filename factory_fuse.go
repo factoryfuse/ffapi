b47f39bfd2b82695e0d27a2d669497fe8684b9ee
Enes Halat <yehalat@outlook.com>
2023-04-05 13:41:32 +0300

    Made the old code work with PostgreSQL.

--- factory_fuse.go
+++ factory_fuse.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 type ArcheId struct {
@@ -29,16 +30,34 @@ type User struct {
   Password string
 }
 
+type DBCred struct {
+  User string
+  Pass string
+  Host string
+  Port string
+  DBName string
+}
+
 func DBInit() (*sql.DB, error) {
-  cfg := mysql.Config{
+  /*cfg := mysql.Config{
     User:   os.Getenv("DBUSER"),
     Passwd: os.Getenv("DBPASS"),
     Net:    "tcp",
     Addr:   "127.0.0.1:3306",
     DBName: "mine",
-  }
+  }*/
 
-  return sql.Open("mysql", cfg.FormatDSN()) 
+  cred := DBCred {
+    User: os.Getenv("DBUSER"),
+    Pass: os.Getenv("DBPASS"),
+    Host: "localhost",
+    Port: "5432",
+    DBName: "ff",
+  };
+
+  connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", cred.User, cred.Pass, cred.Host, cred.Port, cred.DBName);
+
+  return sql.Open("postgres", connStr) 
 }
 
 func MainHandler(c *gin.Context) {
@@ -53,6 +72,7 @@ func OkHandler(c *gin.Context) {
     c.JSON(http.StatusInternalServerError, gin.H{
       "message": "success",
       "serverStatus": "error",
+      "error": if_server_err.Error(),
     })
     return
   }
@@ -74,14 +94,14 @@ func LoginHandler(c *gin.Context) {
 
   var rec_user User
   cur_date := time.Now().Format("2006-01-02 15:04:05")
-  row := db.QueryRow("SELECT id, name, email FROM users WHERE email = ? AND password = ?", user.Email, user.Password)
+  row := db.QueryRow("SELECT id, name, email FROM users WHERE email = $1 AND password = $2", user.Email, user.Password)
   err = row.Scan(&rec_user.Id, &rec_user.Name, &rec_user.Email)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error 1": err.Error()})
 		return
 	}
 
-  _, err = db.Exec("UPDATE users SET last_login = ?, is_logged_in = ? WHERE id = ?", cur_date, "1", rec_user.Id)
+  _, err = db.Exec("UPDATE users SET last_login = $1, is_logged_in = $2 WHERE id = $3", cur_date, "1", rec_user.Id)
   // _, err = db.Exec("INSERT INTO users (last_login, is_logged_in) VALUES (?, ?)", cur_date, "1")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error 2": err.Error()})
@@ -100,7 +120,7 @@ func LogoutHandler(c *gin.Context) {
     return
   }
 
-  _, err = db.Exec("UPDATE users SET is_logged_in = ? WHERE id = ?", "0", id.Id)
+  _, err = db.Exec("UPDATE users SET is_logged_in = $1 WHERE id = $2", "0", id.Id)
   // _, err = db.Exec("INSERT INTO users (last_login, is_logged_in) VALUES (?, ?)", cur_date, "1")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error 2": err.Error()})
@@ -121,7 +141,7 @@ func SignUpHandler(c *gin.Context) {
   cur_date := time.Now().Format("2006-01-02 15:04:05")
   
 
-  result, err := db.Exec("INSERT INTO users (name, email, password, last_login) VALUES (?, ?, ?, ?)", user.Name, user.Email, user.Password, cur_date)
+  result, err := db.Exec("INSERT INTO users (name, email, password, last_login) VALUES ($1, $2, $3, $4)", user.Name, user.Email, user.Password, cur_date)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.20
 
 require github.com/gin-gonic/gin v1.9.0
 
+require github.com/lib/pq v1.10.7
+
 require (
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
@@ -11,7 +13,6 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
-	github.com/go-sql-driver/mysql v1.7.0
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
