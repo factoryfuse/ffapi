c7c24a0b490b134d4081110f33ac14b08a568653
Enes Halat <yehalat@outlook.com>
2023-04-05 17:19:56 +0300

    Added "Session Key" and "/checksession"

--- factory_fuse.go
+++ factory_fuse.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
@@ -48,9 +49,9 @@ func DBInit() (*sql.DB, error) {
   }*/
 
   cred := DBCred {
-    User: os.Getenv("DBUSER"),
-    Pass: os.Getenv("DBPASS"),
-    Host: "localhost",
+    User: os.Getenv("FF_DBUSER"),
+    Pass: os.Getenv("FF_DBPASS"),
+    Host: os.Getenv("FF_DBHOST"),
     Port: "5432",
     DBName: "ff",
   };
@@ -58,6 +59,18 @@ func DBInit() (*sql.DB, error) {
   connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", cred.User, cred.Pass, cred.Host, cred.Port, cred.DBName);
 
   return sql.Open("postgres", connStr) 
+}
+
+func FFCreateAlphaNumericString(length int) string {
+  rand_gen := rand.New(rand.NewSource(time.Now().UnixNano()))
+  const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+  b := make([]byte, length)
+  for i := range b {
+    b[i] = letterBytes[rand_gen.Intn(len(letterBytes))]
+  }
+
+  return string(b)
 }
 
 func MainHandler(c *gin.Context) {
@@ -101,14 +114,21 @@ func LoginHandler(c *gin.Context) {
 		return
 	}
 
-  _, err = db.Exec("UPDATE users SET last_login = $1, is_logged_in = $2 WHERE id = $3", cur_date, "1", rec_user.Id)
-  // _, err = db.Exec("INSERT INTO users (last_login, is_logged_in) VALUES (?, ?)", cur_date, "1")
-	if err != nil {
+  session_id := FFCreateAlphaNumericString(32)
+
+  _, err = db.Exec("UPDATE users SET last_login = $1, is_logged_in = $2 WHERE id = $3", cur_date, "TRUE", rec_user.Id)
+  if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error 2": err.Error()})
 		return
 	}
+  _, err = db.Exec("INSERT INTO active_sessions (session_id, user_name, user_email) VALUES ($1, $2, $3)", session_id, rec_user.Name, rec_user.Email)
+  // _, err = db.Exec("INSERT INTO users (last_login, is_logged_in) VALUES (?, ?)", cur_date, "1")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error 3": err.Error()})
+		return
+	}
 
-  c.JSON(http.StatusOK, gin.H{"message": "success", "id": rec_user.Id})
+  c.JSON(http.StatusOK, gin.H{"message": "success", "id": session_id})
 }
 
 func LogoutHandler(c *gin.Context) {
@@ -120,10 +140,25 @@ func LogoutHandler(c *gin.Context) {
     return
   }
 
-  _, err = db.Exec("UPDATE users SET is_logged_in = $1 WHERE id = $2", "0", id.Id)
+  var user_email string
+
+  err = db.QueryRow("SELECT user_email FROM active_sessions WHERE session_id = $1", id.Id).Scan(&user_email)
+  if err == sql.ErrNoRows {
+    c.JSON(http.StatusBadRequest, gin.H{"error": "invalid session id"})
+    return
+  }
+
+  _, err = db.Exec("UPDATE users SET is_logged_in = $1 WHERE email = $2", "FALSE", user_email)
   // _, err = db.Exec("INSERT INTO users (last_login, is_logged_in) VALUES (?, ?)", cur_date, "1")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error 2": err.Error()})
+		return
+	}
+
+  _, err = db.Exec("DELETE FROM active_sessions WHERE session_id = $1", id.Id)
+  // _, err = db.Exec("INSERT INTO users (last_login, is_logged_in) VALUES (?, ?)", cur_date, "1")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error 3": err.Error()})
 		return
 	}
 
@@ -138,17 +173,45 @@ func SignUpHandler(c *gin.Context) {
     return
   }
 
-  cur_date := time.Now().Format("2006-01-02 15:04:05")
-  
-
-  result, err := db.Exec("INSERT INTO users (name, email, password, last_login) VALUES ($1, $2, $3, $4)", user.Name, user.Email, user.Password, cur_date)
-	if err != nil {
+  _, err := db.Exec("INSERT INTO users (name, email, password) VALUES ($1, $2, $3)", user.Name, user.Email, user.Password)
+  if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-  id, _ := result.LastInsertId()
-  c.JSON(http.StatusOK, gin.H{"message": "success", "id": id})
+  // id, _ := result.LastInsertId()
+  c.JSON(http.StatusOK, gin.H{"message": "success", "extra": "Successfully created user"})
+}
+
+func CheckSessionHandler(c *gin.Context) {
+  var reader map[string]interface{}
+
+  var err error
+  var user_name string
+  var user_email string
+
+  if err = c.BindJSON(&reader); err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    return
+  }
+
+  err = db.QueryRow("SELECT user_name, user_email FROM active_sessions WHERE session_id = $1", reader["id"]).Scan(&user_name, &user_email)
+  if err == sql.ErrNoRows {
+    c.JSON(http.StatusBadRequest, gin.H{
+      "message": "success",
+      "status": "invalid",
+    })
+    return
+  }
+
+  c.JSON(http.StatusOK, gin.H{
+    "message": "success",
+    "status": "open",
+    "user": gin.H{
+      "name": user_name,
+      "email": user_email,
+    },
+  })
 }
 
 var db *sql.DB
@@ -167,5 +230,6 @@ func main() {
   r.POST("/login", LoginHandler)
   r.POST("/logout", LogoutHandler)
   r.POST("/signup", SignUpHandler)
+  r.POST("/checksession", CheckSessionHandler)
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
